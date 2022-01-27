04c3945f22604d2a33e16c82fb1ba80a672f3531
cris-uri199@hotmail.com <cris-uri199@hotmail.com>
2022-01-26 23:58:25 -0600

    initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { ContainerFragment } from './App.styled'
+
+
+class App extends Component {
+  render() {
+    return (
+      <ContainerFragment>
+        <h1>Home Improvement</h1>
+      </ContainerFragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/App.styled.js
@@ -0,0 +1,11 @@
+
+
+const ContainerFragment = styled(React.Fragment)`
+  border: 1px solid red;
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background: blue; 
+`
